33607ec82c78aceea90f0ddde2449b89588dd2a7
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-25 17:20:58 -0500

    Major style additions
    Added header
    Added footer
    Added section where train information is displayed
    Added section where user can add information for a new train

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+(document).ready(function(){
+
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyAJ7GkfZbJOOhvb2rlEyNu6-BryD5-eIFw",
+    authDomain: "train-scheduler-e1c4c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e1c4c.firebaseio.com",
+    projectId: "train-scheduler-e1c4c",
+    storageBucket: "train-scheduler-e1c4c.appspot.com",
+    messagingSenderId: "715656521055"
+    };
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database
+    var database = firebase.database();
+
+    // Initial values
+    var trainName = '';
+    var runTime = '';
+    var
+
+
+    // Capture Button Click
+    $("#add-user").on("click", function(event) {
+        event.preventDefault();
+        
+    });
+
+
+});
