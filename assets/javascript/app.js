404720bb593b042d5daba673e28899adc8b16b8e
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-29 00:31:44 -0500

    Firebase
    Working on realtime database displaying user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
-(document).ready(function(){
+$( document ).ready(function() {
 
     // Initialize Firebase
     var config = {
-    apiKey: "AIzaSyAJ7GkfZbJOOhvb2rlEyNu6-BryD5-eIFw",
-    authDomain: "train-scheduler-e1c4c.firebaseapp.com",
-    databaseURL: "https://train-scheduler-e1c4c.firebaseio.com",
-    projectId: "train-scheduler-e1c4c",
-    storageBucket: "train-scheduler-e1c4c.appspot.com",
-    messagingSenderId: "715656521055"
+    apiKey: 'AIzaSyAJ7GkfZbJOOhvb2rlEyNu6-BryD5-eIFw',
+    authDomain: 'train-scheduler-e1c4c.firebaseapp.com',
+    databaseURL: 'https://train-scheduler-e1c4c.firebaseio.com',
+    projectId: 'train-scheduler-e1c4c',
+    storageBucket: 'train-scheduler-e1c4c.appspot.com',
+    messagingSenderId: '715656521055'
     };
     firebase.initializeApp(config);
 
@@ -15,16 +15,19 @@
     var database = firebase.database();
 
     // Initial values
-    var trainName = '';
-    var runTime = '';
-    var
+    var trainName;
+    var runTime;
 
+    database.ref().on("child_added", function(snapshot) {
+        trainName = snapshot.val().trainName;
+        runTime = snapshot.val().runTime;
+    });
 
     // Capture Button Click
-    $("#add-user").on("click", function(event) {
+    $('#add-user').on('click', function(event) {
         event.preventDefault();
         
     });
 
-
+// End document ready
 });
