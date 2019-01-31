fee2ff57f8565355c733f1d725c38eb5668b89c9
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-31 16:39:38 -0500

    HTML
    Replaced makeshift table using divs with table tags

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,51 +11,103 @@ $(document).ready(function() {
     };
     firebase.initializeApp(config);
 
-    // Create a variable to reference the database
+    // References the database
     var database = firebase.database();
-
-    // Initial values
-    var trainName;
-    var destination;
-    var frequency;
-    var firstTrain;
-    var nextTrain;
-    var minAway;
     
-    function writeTrainData(trainName, destination, firstTrain, frequency) {
-        firebase.database().ref('train-scheduler-e1c4c/' + trainName).set({
-          destination: destination,
-          frequency: frequency,
-          firstTrain: firstTrain
-        });
-    }
-    database.ref().on("child_added", function(snapshot) {
-        trainName = snapshot.val().trainName;
-        destination = snapshot.val().destination;
-        firstTrain = snapshot.val().firstTrain;
-        frequency = snapshot.val().frequency;
-        nextTrain = snapshot.val().nextTrain;
-        minAway = snapshot.val().minAway;
-
-    }, function(errorObject) {
-        console.log("Error: " + errorObject.code);
-    });
-
     // Capture button click
     $('#add-train').on('click', function(event) {
         event.preventDefault();
         
-        // This is where the code is messing up with jQuery 
-        // Not grabbing the right info, so it isnt even hitting firebase
-        var trainName = $('<div class="col-1">').val().trim();
-        var destination = $('<div class="col-2">').val().trim();
-        var frequency = $('<div class="col-3">').val().trim();
-        var firstTrain = '1';
-        var nextTrain = 'a';
-        var minAway = '6';
+        // Grabs user input
+        var trainName = $('#trainName-input').val().trim();
+        var destination = $('#destination-input').val().trim();
+        var frequency = $('#frequency-input').val().trim();
+        var firstTrain = $('#firstTrain-input').val().trim();
         
         writeTrainData(trainName, destination, firstTrain, frequency);
+
+        // Creates local temp object to hold train data
+        var newTrain = {
+            name: trainName,
+            destination: destination,
+            frequency: frequency,
+            firstTrain: firstTrain,
+        }
+
+        // Uploads train data to the database
+        function writeTrainData(trainName, destination, firstTrain, frequency) {
+            firebase.database().ref('train-scheduler-e1c4c/' + newTrain).set({
+                trainName: trainName,
+                destination: destination,
+                frequency: frequency,
+                firstTrain: firstTrain
+            });
+        }
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.frequency);
+        console.log(newTrain.firstTrain);
+
+        alert('train successfully added');
+
+        // Clears form
+        $('#trainName-input').val('');
+        $('#destination-input').val('');
+        $('#frequency-input').val('');
+        $('#firstName-input').val('');
     });
+
+    // Creates Firebase event for adding new train to database and adding new row to html
+    database.ref().on('child_added', function(childSnapshot) {
+        console.log(childSnapshot.val());
+
+        var trainName = childSnapshot.val().trainName;
+        var destination = childSnapshot.val().destination;
+        var firstTrain = childSnapshot.val().firstTrain;
+        var frequency = childSnapshot.val().frequency;
+        var nextTrain = childSnapshot.val().nextTrain;
+        var minAway = childSnapshot.val().minAway;
+
+        // Train info
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTrain);
+        console.log(frequency);
+        console.log(nextTrain);
+        console.log(minAway);
+        
+// ---------------------------------------------
+        // Moment js
+
+
+// ---------------------------------------------
+
+        // Creates new row
+        var newRow = $('<tr>').append(
+            $('<td>').text(trainName),
+            $('<td>').text(destination),
+            $('<td>').text(frequency),
+            $('<td>').text(firstTrain),
+            $('<td>').text(nextTrain),
+            $('<td>').text(minAway)
+        );
+            
+        // Appends new row to table
+        $('#train-table').append(newRow);
+
+        // TODO:where should this go?
+        }, function(errorObject) {
+            console.log('Error: ' + errorObject.code);
+    });
+
+
+    // function writeTrainData(trainName, destination, firstTrain, frequency) {
+    //     firebase.database().ref('train-scheduler-e1c4c/' + trainName).set({
+    //       destination: destination,
+    //       frequency: frequency,
+    //       firstTrain: firstTrain
+    //     });
+    // }
 
 // End document ready
 });
